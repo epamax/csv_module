5d67d9ba1c1adac830547af0723cb63f5921ab2c
Max Collins <collins.maxwell@epa.gov>
2022-12-15 18:48:19 -0500

    deep copy example prac

--- deep_copy_Struct.go
+++ deep_copy_Struct.go
@@ -20,17 +20,17 @@ func CopyStruct (emptyStruct struct{}) any {
 }
 
 func main() {
-	// x := "Original"
-	// p := DemoStruct{value: 20, name: "Struct1", s_pointer: &x}
-	// q := p
-	// q.s_pointer = nil
-	// y := *p.s_pointer
-	// q.s_pointer = &y
-	// *q.s_pointer = "Altered"
+	x := "Original"
+	p := DemoStruct{value: 20, name: "Struct1", s_pointer: &x}
+	q := p
+	q.s_pointer = nil
+	y := *p.s_pointer
+	q.s_pointer = &y
+	*q.s_pointer = "Altered"
 
-	// fmt.Println(*p.s_pointer)
-	// fmt.Println(*q.s_pointer)
-	// fmt.Println(&p.s_pointer == &q.s_pointer)
+	fmt.Println(*p.s_pointer)
+	fmt.Println(*q.s_pointer)
+	fmt.Println(&p.s_pointer == &q.s_pointer)
 	copyStruct := CopyStruct(DemoStruct{})
 	fmt.Println(copyStruct)
 }
